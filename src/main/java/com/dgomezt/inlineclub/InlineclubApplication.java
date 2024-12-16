5a1b9cc8e5842696828b602b15c365d9bdc2beaa
Daniel Gómez Torquemada <daniel.gomez@habber.com>
2024-12-16 12:24:59 +0100

    first commit

---
+++ src/main/java/com/dgomezt/inlineclub/InlineclubApplication.java
@@ -0,0 +1,13 @@
+package com.dgomezt.inlineclub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InlineclubApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InlineclubApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/dgomezt/inlineclub/InlineclubApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dgomezt.inlineclub;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InlineclubApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
